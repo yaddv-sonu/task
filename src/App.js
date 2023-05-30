0795f8422e08d4a77d808697624a9c1955403352
yaddv-sonu <sonu.yadav@pec.edu>
2023-05-30 23:11:22 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Sidebar.css'; // Import the CSS file for styling
 
-function App() {
+const Sidebar = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() !== '') {
+      const newMessage = {
+        id: messages.length + 1,
+        content: inputValue,
+        timestamp: new Date().toLocaleTimeString()
+      };
+
+      setMessages([...messages, newMessage]);
+      setInputValue('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sidebar">
+      <div className="chat-header">
+        <h2>Chat</h2>
+      </div>
+      <div className="chat-messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <div className="avatar"></div>
+            <div className="message-content">
+              <span className="content">{message.content}</span>
+              <span className="timestamp">{message.timestamp}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Sidebar;
